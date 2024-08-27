0404832579e3de903e04fd094c481caad03d996f
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-08-27 17:01:07 -0300

    feat: adicionando setup inicial para implementacao do login

--- src/ProjectE.Core/Entities/Customer.cs
+++ src/ProjectE.Core/Entities/Customer.cs
@@ -8,7 +8,7 @@ namespace ProjectE.Core.Entities
 {
     public class Customer : Entity
     {
-        public Customer(string name, string email, DateTime birthDate, bool active) : base()
+        public Customer(string name, string email, DateTime birthDate, bool active, string password, string role) : base()
         {
             Name = name;
             Email = email;
@@ -18,12 +18,16 @@ namespace ProjectE.Core.Entities
             ProjectComments = [];
             OwnedProjects = [];
             FreelanceProjects = [];
+            Password = password;
+            Role = role;
         }
 
         public string Name { get; private set; }
         public string Email { get; private set; }
         public DateTime BirthDate { get; set; }
         public bool Active { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
         public List<Skill> Skills { get; private set; }
         public List<ProjectComment> ProjectComments { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
---
+++ src/ProjectE.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProjectE.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectE.Infrastructure.Auth
+{
+    public class AuthService(IConfiguration configuration) : IAuthService
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        public string ComputeSha256Hash(string password)
+        {
+            using(SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                
+                for(int i = 0; i < bytes.Length; i++) builder.Append(bytes[i].ToString("x2"));
+               
+                return builder.ToString();
+            }
+        }
+
+        public string GenerateJwtToken(string email, string role)
+        {
+            var issuer = _configuration["Jwt:Issuer"] ?? string.Empty;
+            var audience = _configuration["Jwt:Audience"] ?? string.Empty;
+            var key = _configuration["Jwt:Key"] ?? string.Empty;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+            {
+                new Claim("userName", email),
+                new Claim(ClaimTypes.Role, role),
+            };
+
+            var token = new JwtSecurityToken(issuer, audience, claims, null, DateTime.Now.AddHours(8), credentials);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var jwt = tokenHandler.WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
